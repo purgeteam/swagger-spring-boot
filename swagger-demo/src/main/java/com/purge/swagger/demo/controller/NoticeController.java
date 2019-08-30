33a2058adb63c9339a672b096259da82f8f40653
Purgeyao <zhaoy@deepblueai.com>
2019-08-30 15:07:49 +0800

    添加文档

---
+++ swagger-demo/src/main/java/com/purge/swagger/demo/controller/NoticeController.java
@@ -0,0 +1,33 @@
+package com.purge.swagger.demo.controller;
+
+import com.purge.swagger.demo.model.Notice;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author purgeyao
+ * @since 1.0
+ */
+@RestController
+@RequestMapping("/notices")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Api(tags = "公告相关接口", description = "公告相关接口")
+public class NoticeController {
+
+  /**
+   * 查询最新的一条公告
+   *
+   * @return 公告列表
+   */
+  @GetMapping("/newest")
+  @ApiOperation(value = "查询最新的一条公告", notes = "用于：公告")
+  public Notice findNewest() {
+    return new Notice();
+  }
+
+}
---
+++ swagger-demo/src/main/java/com/purge/swagger/demo/model/Notice.java
@@ -0,0 +1,33 @@
+package com.purge.swagger.demo.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author purgeyao
+ * @since 1.0
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@ApiModel("公告")
+public class Notice {
+
+  /**
+   * ID
+   */
+  @ApiModelProperty("id")
+  private Integer id;
+
+  /**
+   * 公告内容
+   */
+  @ApiModelProperty("公告内容")
+  private String content;
+
+}
